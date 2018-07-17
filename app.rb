9088ccc19e460d45b0bee8ff2edb9ea03160043d
Ashley Minor <aminor@kipphouston.org>
2018-07-17 17:20:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,17 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  
   get '/name' do
-    "My name is Ashley!"
+    "My name is Ashley Minor."
   end
   
   get '/hometown' do
     "My hometown is Plano, TX."
   end
   
-  get '/favorite-song'
-  "My favorite song is Sun is Shining by Axwell X Ingrosso."
+  get '/favorite-song' do
+    "My favorite song is Sun is Shining - Axwell X Ingrosso."
   end
-
+  
 end
